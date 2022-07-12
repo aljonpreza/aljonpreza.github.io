aaec150dfa79ac778f6bcf38cb898cc271c3a6f0
aljonpreza <aljonpreza@yahoo.com>
2022-07-11 20:36:22 -1000

    fix javascript

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,27 @@
-$(window).on("load", function () {
-    $(window).scroll(function () {
-        // Get the height of the window
-        var windowHeight = $(this).scrollTop() + $(this).innerHeight();
-
-        // For each .fade class, calculate the object's top position
-        // If the object's position is less than window's height, it is in view
-        // Check if opacity is 0, then fade to 1 with slow speed
-        $('.--animated').each(function () {
-            var objectTop = $(this).offset().top + ($(this).outerHeight() / 4);
-            if (objectTop < windowHeight && !$(this).hasClass('--fadeIn')) {
-                $(this).addClass("--fadeIn");
-            }
-        });
-    }).scroll();
-
-    $(window).scroll(function () {
-        var $nav = $(".site-header");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    }).scroll();
+$(document).ready(function () {
+    $(window).on("load", function () {
+        $(window).scroll(function () {
+            // Get the height of the window
+            var windowHeight = $(this).scrollTop() + $(this).innerHeight();
+    
+            // For each .fade class, calculate the object's top position
+            // If the object's position is less than window's height, it is in view
+            // Check if opacity is 0, then fade to 1 with slow speed
+            $('.--animated').each(function () {
+                var objectTop = $(this).offset().top + ($(this).outerHeight() / 4);
+                if (objectTop < windowHeight && !$(this).hasClass('--fadeIn')) {
+                    $(this).addClass("--fadeIn");
+                }
+            });
+        }).scroll();
+    
+        $(window).scroll(function () {
+            var $nav = $(".site-header");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        }).scroll();
+    });
 });
+
 
 $(document).ready(function () {
     const swiper = new Swiper('.swiper', {
